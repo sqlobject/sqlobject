e6f33a34514cad7103551b7d21eaa20f85cee8ed
Ian Bicking <ianb@colorstudy.com>
2004-02-11 01:30:58 +0000

    Added some backward-compatible *Connection constructors
    git-svn-id: http://svn.colorstudy.com/trunk/SQLObject@14 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/__init__.py
+++ sqlobject/__init__.py
@@ -7,16 +7,52 @@ from include import validators
 from dbconnection import connectionForURI
 
 ## Each of these imports allows the driver to install itself
+## Then we set up some backward compatibility
+
+def _warn(msg):
+    import warnings
+    warnings.warn(msg, warnings.DeprecationWarning, stacklevel=2)
 
 import firebird
+_firebird = firebird
 del firebird
+def FirebirdConnection(*args, **kw):
+    _warn('FirebirdConnection is deprecated; use connectionForURI("firebird://...") or "from sqlobject.firebird import builder; FirebirdConnection = builder()"')
+    _firebird.builder()(*args, **kw)
+
 import mysql
+_mysql = mysql
 del mysql
+def MySQLConnection(*args, **kw):
+    _warn('MySQLConnection is deprecated; use connectionForURI("mysql://...") or "from sqlobject.mysql import builder; MySQLConnection = builder()"')
+    _mysql.builder()(*args, **kw)
+
 import postgres
+_postgres = postgres
 del postgres
+def PostgresConnection(*args, **kw):
+    _warn('PostgresConnection is deprecated; use connectionForURI("postgres://...") or "from sqlobject.postgres import builder; PostgresConnection = builder()"')
+    _postgres.builder()(*args, **kw)
+
 import sqlite
+_sqlite = sqlite
 del sqlite
+def SQLiteConnection(*args, **kw):
+    _warn('SQLiteConnection is deprecated; use connectionForURI("sqlite://...") or "from sqlobject.sqlite import builder; SQLiteConnection = builder()"')
+    _sqlite.builder()(*args, **kw)
+
 import dbm
+_dbm = dbm
 del dbm
+def DBMConnection(*args, **kw):
+    _warn('DBMConnection is deprecated; use connectionForURI("dbm://...") or "from sqlobject.dbm import builder; DBMConnection = builder()"')
+    _dbm.builder()(*args, **kw)
+
 import sybase
+_sybase = sybase
 del sybase
+def SybaseConnection(*args, **kw):
+    _warn('SybaseConnection is deprecated; use connectionForURI("sybase://...") or "from sqlobject.sybase import builder; SybaseConnection = builder()"')
+    _sybase.builder()(*args, **kw)
+
+
