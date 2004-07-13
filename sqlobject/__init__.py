014eeb8d3d7c0716aa5bcee8199f0e6399edfd82
Karl Chen <quarl@0d56.fdybowrpg.z.quarl.org>
2004-07-13 10:29:32 +0000

    fix import problems; use "import as" construct
    git-svn-id: http://svn.colorstudy.com/trunk/SQLObject@161 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/__init__.py
+++ sqlobject/__init__.py
@@ -13,52 +13,37 @@ def _warn(msg):
     import warnings
     warnings.warn(msg, DeprecationWarning, stacklevel=2)
 
-import firebird
-_firebird = firebird
-del firebird
+import firebird as _firebird
 def FirebirdConnection(*args, **kw):
     _warn('FirebirdConnection is deprecated; use connectionForURI("firebird://...") or "from sqlobject.firebird import builder; FirebirdConnection = builder()"')
-    _firebird.builder()(*args, **kw)
+    return _firebird.builder()(*args, **kw)
 
-import mysql
-_mysql = mysql
-del mysql
+import mysql as _mysql
 def MySQLConnection(*args, **kw):
     _warn('MySQLConnection is deprecated; use connectionForURI("mysql://...") or "from sqlobject.mysql import builder; MySQLConnection = builder()"')
-    _mysql.builder()(*args, **kw)
+    return _mysql.builder()(*args, **kw)
 
-import postgres
-_postgres = postgres
-del postgres
+import postgres as _postgres
 def PostgresConnection(*args, **kw):
     _warn('PostgresConnection is deprecated; use connectionForURI("postgres://...") or "from sqlobject.postgres import builder; PostgresConnection = builder()"')
-    _postgres.builder()(*args, **kw)
+    return _postgres.builder()(*args, **kw)
 
-import sqlite
-_sqlite = sqlite
-del sqlite
+import sqlite as _sqlite
 def SQLiteConnection(*args, **kw):
     _warn('SQLiteConnection is deprecated; use connectionForURI("sqlite://...") or "from sqlobject.sqlite import builder; SQLiteConnection = builder()"')
-    _sqlite.builder()(*args, **kw)
+    return _sqlite.builder()(*args, **kw)
 
-import dbm
-_dbm = dbm
-del dbm
+import dbm as _dbm
 def DBMConnection(*args, **kw):
     _warn('DBMConnection is deprecated; use connectionForURI("dbm://...") or "from sqlobject.dbm import builder; DBMConnection = builder()"')
-    _dbm.builder()(*args, **kw)
+    return _dbm.builder()(*args, **kw)
 
-import sybase
-_sybase = sybase
-del sybase
+import sybase as _sybase
 def SybaseConnection(*args, **kw):
     _warn('SybaseConnection is deprecated; use connectionForURI("sybase://...") or "from sqlobject.sybase import builder; SybaseConnection = builder()"')
-    _sybase.builder()(*args, **kw)
+    return _sybase.builder()(*args, **kw)
 
-
-import maxdb
-_maxdb = maxdb
-del maxdb
+import maxdb as _maxdb
 def MaxdbConnection(*args, **kw):
     _warn('MaxdbConnection is deprecated; use connectionForURI("maxdb://...") or "from sqlobject.maxdb import builder; MaxdbConnection = builder()"')
-    _maxdb.builder()(*args, **kw)
+    return _maxdb.builder()(*args, **kw)
