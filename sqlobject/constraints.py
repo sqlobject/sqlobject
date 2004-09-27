0d5297f26fdf8486b1c23d77ee36ff14277134b6
Ian Bicking <ianb@colorstudy.com>
2004-09-27 17:33:17 +0000

    Small bug fixes
    git-svn-id: http://svn.colorstudy.com/trunk/SQLObject@227 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/constraints.py
+++ sqlobject/constraints.py
@@ -15,7 +15,7 @@ class BadValue(ValueError):
         self.obj = repr(obj)
         self.value = repr(value)
         fullDesc = "%s.%s %s (you gave: %s)" \
-                   % (obj.__name__, col.name, desc, repr(value))
+                   % (obj, col.name, desc, value)
         ValueError.__init__(self, fullDesc, *args)
 
 def isString(obj, col, value):
@@ -45,7 +45,7 @@ class InList:
 
     def __call__(self, obj, col, value):
         if value not in self.list:
-            raise BadValue("accepts only values in %s" % self.list,
+            raise BadValue("accepts only values in %s" % repr(self.list),
                            obj, col, value)
 
 class MaxLength:
@@ -54,7 +54,12 @@ class MaxLength:
         self.length = length
 
     def __call__(self, obj, col, value):
-        if len(value) > self.length:
+        try:
+            length = len(value)
+        except TypeError:
+            raise BadValue("object does not have a length",
+                           obj, col, value)
+        if length > self.length:
             raise BadValue("must be shorter in length than %s"
                            % self.length,
                            obj, col, value)
