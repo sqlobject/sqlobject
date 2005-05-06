00c99cf1a4caf43bb8f33a02ea6b648232f28d94
Ian Bicking <ianb@colorstudy.com>
2005-05-06 17:07:38 +0000

    py.test was catching class named Test*
    git-svn-id: http://svn.colorstudy.com/trunk/SQLObject@776 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/tests/test_aliases.py
+++ sqlobject/tests/test_aliases.py
@@ -6,22 +6,22 @@ from sqlobject.tests.dbtest import *
 ## Table aliases and self-joins
 ########################################
 
-class TestJoinAlias(SQLObject):
+class JoinAlias(SQLObject):
     name = StringCol()
     parent = StringCol()
 
 def test_1syntax():
-    setupClass(TestJoinAlias)
-    alias = Alias(TestJoinAlias)
-    select = TestJoinAlias.select(TestJoinAlias.q.parent == alias.q.name)
+    setupClass(JoinAlias)
+    alias = Alias(JoinAlias)
+    select = JoinAlias.select(JoinAlias.q.parent == alias.q.name)
     assert str(select) == \
-        "SELECT test_join_alias.id, test_join_alias.name, test_join_alias.parent FROM test_join_alias, test_join_alias AS test_join_alias_alias1 WHERE (test_join_alias.parent = test_join_alias_alias1.name)"
+        "SELECT join_alias.id, join_alias.name, join_alias.parent FROM join_alias AS join_alias_alias1, join_alias WHERE (join_alias.parent = join_alias_alias1.name)"
 
 def test_2perform_join():
-    setupClass(TestJoinAlias)
-    TestJoinAlias(name="grandparent", parent=None)
-    TestJoinAlias(name="parent", parent="grandparent")
-    TestJoinAlias(name="child", parent="parent")
-    alias = Alias(TestJoinAlias)
-    select = TestJoinAlias.select(TestJoinAlias.q.parent == alias.q.name)
+    setupClass(JoinAlias)
+    JoinAlias(name="grandparent", parent=None)
+    JoinAlias(name="parent", parent="grandparent")
+    JoinAlias(name="child", parent="parent")
+    alias = Alias(JoinAlias)
+    select = JoinAlias.select(JoinAlias.q.parent == alias.q.name)
     assert select.count() == 2
