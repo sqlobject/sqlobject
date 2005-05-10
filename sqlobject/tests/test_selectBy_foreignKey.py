3454b22abb71a0d959aa273efbbdc3069403a795
Ian Bicking <ianb@colorstudy.com>
2005-05-10 17:38:24 +0000

    Fixed (another) duplicate name
    git-svn-id: http://svn.colorstudy.com/trunk/SQLObject@788 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/tests/test_selectBy_foreignKey.py
+++ sqlobject/tests/test_selectBy_foreignKey.py
@@ -4,30 +4,30 @@ from sqlobject.tests.dbtest import *
 class TestComposerKey(SQLObject):
     name = StringCol()
 
-class TestWork(SQLObject):
+class TestWorkKey(SQLObject):
     class sqlmeta:
         idName = "work_id"
 
-    composer = ForeignKey('TestComposer')
+    composer = ForeignKey('TestComposerKey')
     title = StringCol()
 
 def test1():
     setupClass([TestComposerKey,
-                TestWork])
+                TestWorkKey])
 
     c = TestComposerKey(name='Mahler, Gustav')
-    w1 = TestWork(composer=c, title='Symphony No. 9')
-    w2 = TestWork(composer=None, title=None)
+    w1 = TestWorkKey(composer=c, title='Symphony No. 9')
+    w2 = TestWorkKey(composer=None, title=None)
 
     # Select by usual way
-    s = TestWork.selectBy(composerID=c.id, title='Symphony No. 9')
+    s = TestWorkKey.selectBy(composerID=c.id, title='Symphony No. 9')
     assert s[0]==w1
     # selectBy object
-    s = TestWork.selectBy(composer=c, title='Symphony No. 9')
+    s = TestWorkKey.selectBy(composer=c, title='Symphony No. 9')
     assert s[0]==w1
     # selectBy id
-    s = TestWork.selectBy(id=w1.id)
+    s = TestWorkKey.selectBy(id=w1.id)
     assert s[0]==w1
     # is None handled correctly?
-    s = TestWork.selectBy(composer=None, title=None)
+    s = TestWorkKey.selectBy(composer=None, title=None)
     assert s[0]==w2
