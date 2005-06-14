5a19f13b8a7009665c34a296684d0e5c2b799778
Oleg Broytman <phd@phdru.name>
2005-06-14 13:04:22 +0000

    Fixed a very subtle bug - self.items is being appended.
    git-svn-id: http://svn.colorstudy.com/trunk/SQLObject@816 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/sqlbuilder.py
+++ sqlobject/sqlbuilder.py
@@ -445,7 +445,7 @@ class Select(SQLExpression):
         select = "SELECT %s" % ", ".join([sqlrepr(v, db) for v in self.items])
 
         tables = {}
-        things = self.items
+        things = self.items[:]
         if self.whereClause is not NoDefault:
             things.append(self.whereClause)
         for thing in things:
