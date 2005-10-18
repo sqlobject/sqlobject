a101f7ad5936356b05ec10f3422af8cd8ca2d3fa
Oleg Broytman <phd@phdru.name>
2005-10-18 13:52:14 +0000

    A number of minor enhancements and fixes.
    git-svn-id: http://svn.colorstudy.com/SQLObject/trunk@1118 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/tests/test_datetime.py
+++ sqlobject/tests/test_datetime.py
@@ -1,11 +1,6 @@
 from sqlobject import *
 from sqlobject.tests.dbtest import *
 from sqlobject import col
-if default_datetime_implementation == DATETIME_IMPLEMENTATION:
-    from datetime import datetime
-    now = datetime.now
-elif default_datetime_implementation == MXDATETIME_IMPLEMENTATION:
-    from mx.DateTime import now
 
 ########################################
 ## Date/time columns
@@ -14,39 +9,57 @@ elif default_datetime_implementation == MXDATETIME_IMPLEMENTATION:
 if datetime_available:
     col.default_datetime_implementation = DATETIME_IMPLEMENTATION
     from datetime import date, datetime
+    now = datetime.now
 
     class DateTime1(SQLObject):
-        col1 = DateCol()
-        col2 = DateTimeCol()
+        col1 = DateTimeCol()
+        col2 = DateCol()
 
     def test_dateTime():
         setupClass(DateTime1)
         _now = now()
-        dt1 = DateTime1(col1=_now, col2=_now)
-        assert isinstance(dt1.col1, date)
-        assert isinstance(dt1.col2, datetime)
+        _today = date.today()
+        dt1 = DateTime1(col1=_now, col2=_today)
 
-        today_str = _now.strftime("%Y-%m-%d")
-        now_str = _now.strftime("%Y-%m-%d %T")
-        assert str(dt1.col1) == today_str
-        assert str(dt1.col2) == now_str
+        assert isinstance(dt1.col1, datetime)
+        assert dt1.col1.year == _now.year
+        assert dt1.col1.month == _now.month
+        assert dt1.col1.day == _now.day
+        assert dt1.col1.hour == _now.hour
+        assert dt1.col1.minute == _now.minute
+        assert dt1.col1.second == int(_now.second)
+
+        assert isinstance(dt1.col2, date)
+        assert dt1.col2.year == _today.year
+        assert dt1.col2.month == _today.month
+        assert dt1.col2.day == _today.day
 
 if mxdatetime_available:
     col.default_datetime_implementation = MXDATETIME_IMPLEMENTATION
+    from mx.DateTime import now
 
     class DateTime2(SQLObject):
-        col1 = DateCol()
-        col2 = DateTimeCol()
+        col1 = DateTimeCol()
+        col2 = DateCol(dateFormat="%Y-%m-%d %H:%M:%S")
+        # mxDateTime does not have a separate date type
 
     def test_mxDateTime():
         setupClass(DateTime2)
         _now = now()
         dt2 = DateTime2(col1=_now, col2=_now)
+
         assert isinstance(dt2.col1, col.DateTimeType)
+        assert dt2.col1.year == _now.year
+        assert dt2.col1.month == _now.month
+        assert dt2.col1.day == _now.day
+        assert dt2.col1.hour == _now.hour
+        assert dt2.col1.minute == _now.minute
+        assert dt2.col1.second == int(_now.second)
+
         assert isinstance(dt2.col2, col.DateTimeType)
-
-        today_str = _now.strftime("%Y-%m-%d 00:00:00.00")
-        now_str = _now.strftime("%Y-%m-%d %T.00")
-        assert str(dt2.col1) == today_str
-        assert str(dt2.col2) == now_str
-
+        assert dt2.col2.year == _now.year
+        assert dt2.col2.month == _now.month
+        assert dt2.col2.day == _now.day
+        assert dt2.col1.hour == _now.hour
+        assert dt2.col1.minute == _now.minute
+        assert dt2.col1.second == int(_now.second)
