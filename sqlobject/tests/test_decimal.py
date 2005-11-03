506cbcf46cba507711d703ad1577abb9305791b8
Oleg Broytman <phd@phdru.name>
2005-11-03 16:27:43 +0000

    A little patch for psycopg2 by Humberto Diogenes <adm.humberto@digi.com.br>.
    git-svn-id: http://svn.colorstudy.com/SQLObject/trunk@1206 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/tests/test_decimal.py
+++ sqlobject/tests/test_decimal.py
@@ -13,18 +13,23 @@ except ImportError:
 class DecimalTable(SQLObject):
     col1 = DecimalCol(size=6, precision=4)
 
-def test_1decimal():
-    if not supports('decimalColumn'):
-        return
-    setupClass(DecimalTable)
-    d = DecimalTable(col1=21.12)
-    assert d.col1 == 21.12
-
-if Decimal and supports('decimalColumn'):
-    def test_2Decimal():
+if supports('decimalColumn'):
+    def test_1decimal():
+        """Tests new instance with a float in col1."""
         setupClass(DecimalTable)
-        d = DecimalTable(col1=Decimal("21.12"))
-        if isinstance(d.col1, Decimal):
+        d = DecimalTable(col1=21.12)
+        # psycopg2 returns float as Decimal
+        if Decimal and isinstance(d.col1, Decimal):
             assert d.col1 == Decimal("21.12")
         else:
             assert d.col1 == 21.12
+
+    if Decimal:
+        def test_2Decimal():
+            """Tests new instance with a Decimal in col1."""
+            setupClass(DecimalTable)
+            d = DecimalTable(col1=Decimal("21.12"))
+            if isinstance(d.col1, Decimal):
+                assert d.col1 == Decimal("21.12")
+            else:
+                assert d.col1 == 21.12
