ca6a5803fa4b88cd53c099a6814890968c69dded
Oleg Broytman <phd@phdru.name>
2006-10-30 17:33:51 +0000

    A patch by michelts <michelts@gmail.com>: convert timedelta from MySQLdb to time.
    git-svn-id: http://svn.colorstudy.com/SQLObject/trunk@2045 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/col.py
+++ sqlobject/col.py
@@ -1127,6 +1127,12 @@ if datetime_available:
         def to_python(self, value, state):
             if isinstance(value, datetime.time):
                 return value
+            if isinstance(value, datetime.timedelta):
+                if value.days:
+                    raise validators.Invalid(
+                        "the value for the TimeCol '%s' must has days=0, it has days=%d" %
+                            (self.name, value.days), value, state)
+                return datetime.time(*time.gmtime(value.seconds)[3:6])
             value = super(TimeValidator, self).to_python(value, state)
             if isinstance(value, datetime.datetime):
                 value = value.time()
