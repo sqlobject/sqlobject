bda72e56cbc08ce453e186cf826d3b1ecf70b170
Ian Bicking <ianb@colorstudy.com>
2006-12-06 16:41:59 +0000

    Changed locations for devel docs; removed blog link
    git-svn-id: http://svn.colorstudy.com/SQLObject/trunk@2118 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ For development see the `subversion repository
         ],
       author="Ian Bicking",
       author_email="ianb@colorstudy.com",
-      url="http://sqlobject.org",
+      url="http://sqlobject.org/devel/",
       download_url="http://cheeseshop.python.org/pypi/SQLObject/",
       license="LGPL",
       packages=["sqlobject"] + ['sqlobject.%s' % package for package in subpackages],
