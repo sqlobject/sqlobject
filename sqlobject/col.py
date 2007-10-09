48be7123e14b34c0aa910d510340ccfae62226f5
Oleg Broytman <phd@phdru.name>
2007-10-09 15:44:14 +0000

    Fixed StringCol and UnicodeCol: use sqlType with MSSQL.
    git-svn-id: http://svn.colorstudy.com/SQLObject/trunk@3027 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/col.py
+++ sqlobject/col.py
@@ -475,6 +475,8 @@ class SOStringLikeCol(SOCol):
         return type
 
     def _mssqlType(self):
+        if self.customSQLType is not None:
+            return self.customSQLType
         if not self.length:
             type = 'varchar(4000)'
         elif self.varchar:
