6e7abcd06420a8ca676c64ca061675d07949fe9c
Oleg Broytman <phd@phdru.name>
2009-08-13 16:32:31 +0000

    Fixed circular import problem.
    git-svn-id: http://svn.colorstudy.com/SQLObject/trunk@3950 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/__init__.py
+++ sqlobject/__init__.py
@@ -2,11 +2,11 @@
 SQLObject 0.12
 """
 
-from dbconnection import connectionForURI
 from col import *
 from index import *
 from joins import *
 from main import *
 from sqlbuilder import AND, OR, NOT, IN, LIKE, RLIKE, DESC, CONTAINSSTRING, const, func
 from styles import *
+from dbconnection import connectionForURI
 import dberrors
