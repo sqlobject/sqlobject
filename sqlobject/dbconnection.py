bad55cc924e101d37cedf4911c11e6f67801f3fc
Oleg Broytman <phd@phdru.name>
2009-09-25 14:50:07 +0000

    Merged a bugfix in logging to console fro branch 0.11, rev. 3993.
    git-svn-id: http://svn.colorstudy.com/SQLObject/trunk@3994 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/dbconnection.py
+++ sqlobject/dbconnection.py
@@ -28,27 +28,33 @@ def _closeConnection(ref):
         conn.close()
 
 class ConsoleWriter:
-    def __init__(self, loglevel):
+    def __init__(self, connection, loglevel):
         # loglevel: None or empty string for stdout; or 'stderr'
         self.loglevel = loglevel or "stdout"
+        self.dbEncoding = getattr(connection, "dbEncoding", "ascii")
     def write(self, text):
         logfile = getattr(sys, self.loglevel)
+        if isinstance(text, unicode):
+            try:
+                text = text.encode(self.dbEncoding)
+            except UnicodeEncodeError:
+                text = repr(text)[2:-1]
         logfile.write(text + '\n')
 
 class LogWriter:
-    def __init__(self, logger, loglevel):
+    def __init__(self, connection, logger, loglevel):
         self.logger = logger
         self.loglevel = loglevel
         self.logmethod = getattr(logger, loglevel)
     def write(self, text):
         self.logmethod(text)
 
-def makeDebugWriter(loggerName, loglevel):
+def makeDebugWriter(connection, loggerName, loglevel):
     if not loggerName:
-        return ConsoleWriter(loglevel)
+        return ConsoleWriter(connection, loglevel)
     import logging
     logger = logging.getLogger(loggerName)
-    return LogWriter(logger, loglevel)
+    return LogWriter(connection, logger, loglevel)
 
 class Boolean(object):
     """A bool class that also understands some special string keywords (yes/no, true/false, on/off, 1/0)"""
@@ -70,7 +76,7 @@ class DBConnection:
         self.debug = Boolean(debug)
         self.debugOutput = Boolean(debugOutput)
         self.debugThreading = Boolean(debugThreading)
-        self.debugWriter = makeDebugWriter(logger, loglevel)
+        self.debugWriter = makeDebugWriter(self, logger, loglevel)
         self.doCache = Boolean(cache)
         self.cache = CacheSet(cache=self.doCache)
         self.style = style
