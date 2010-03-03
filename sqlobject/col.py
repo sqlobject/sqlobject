bb135768c8bf6f255f0cf5b2e0ee7e5ab8ceeaf3
Oleg Broytman <phd@phdru.name>
2010-03-03 18:37:57 +0000

    Test connection-specific Binary type and buffer type.
    git-svn-id: http://svn.colorstudy.com/SQLObject/trunk@4119 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/col.py
+++ sqlobject/col.py
@@ -507,6 +507,9 @@ class SOStringLikeCol(SOCol):
             return self._sqlType()
 
 
+# Special marker to test the dataType is a backend-specific Binary
+testDataTypeBinary = object()
+
 class StringValidator(validators.Validator):
 
     def to_python(self, value, state):
@@ -516,20 +519,32 @@ class StringValidator(validators.Validator):
             connection = state.soObject._connection
             dbEncoding = getattr(connection, "dbEncoding", None) or "ascii"
             return value.encode(dbEncoding)
-        if isinstance(value, (str, sqlbuilder.SQLExpression)):
+        if isinstance(value, (str, buffer, sqlbuilder.SQLExpression)):
             return value
-        if self.dataType and isinstance(value, self.dataType):
-            return value
+        if self.dataType:
+            if self.dataType is testDataTypeBinary:
+                connection = state.soObject._connection
+                dataType = connection._binaryType
+            else:
+                dataType = self.dataType
+            if isinstance(value, dataType):
+                return value
         raise validators.Invalid("expected a str in the StringCol '%s', got %s %r instead" % \
             (self.name, type(value), value), value, state)
 
     def from_python(self, value, state):
         if value is None:
             return None
-        if isinstance(value, (str, sqlbuilder.SQLExpression)):
+        if isinstance(value, (str, buffer, sqlbuilder.SQLExpression)):
             return value
-        if self.dataType and isinstance(value, self.dataType):
-            return value
+        if self.dataType:
+            if self.dataType is testDataTypeBinary:
+                connection = state.soObject._connection
+                dataType = connection._binaryType
+            else:
+                dataType = self.dataType
+            if isinstance(value, dataType):
+                return value
         if isinstance(value, unicode):
             connection = state.soObject._connection
             dbEncoding = getattr(connection, "dbEncoding", None) or "ascii"
@@ -1469,7 +1484,7 @@ class SOBLOBCol(SOStringCol):
 
     def createValidators(self):
         return [BinaryValidator(name=self.name)] + \
-            super(SOBLOBCol, self).createValidators(dataType=buffer)
+            super(SOBLOBCol, self).createValidators(dataType=testDataTypeBinary)
 
     def _mysqlType(self):
         length = self.length
