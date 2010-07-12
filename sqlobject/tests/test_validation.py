8a33effe9d5608a22d2aa1c209130ab4988d26cf
Oleg Broytman <phd@phdru.name>
2010-07-12 15:47:53 +0000

    Test instances of classes with special methods.
    git-svn-id: http://svn.colorstudy.com/SQLObject/trunk@4205 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/tests/test_validation.py
+++ sqlobject/tests/test_validation.py
@@ -8,13 +8,35 @@ from formencode import validators
 
 class SOValidation(SQLObject):
 
-    name = StringCol(validator=validators.PlainText(), default='x', dbName='name_col')
+    name = StringCol(validator=validators.PlainText(),
+        default='x', dbName='name_col')
     name2 = StringCol(validator=validators.ConfirmType(type=str), default='y')
     name3 = IntCol(validator=validators.Wrapper(fromPython=int), default=100)
     name4 = FloatCol(default=2.718)
     name5 = PickleCol(default=None)
     name6 = BoolCol(default=None)
     name7 = UnicodeCol(default=None)
+    name8 = IntCol(default=None)
+
+class SOValidationTest(object):
+    def __init__(self, value):
+        self.value = value
+
+class SOValidationTestUnicode(SOValidationTest):
+    def __unicode__(self):
+        return self.value
+
+class SOValidationTestInt(SOValidationTest):
+    def __int__(self):
+        return self.value
+
+class SOValidationTestBool(SOValidationTest):
+    def __nonzero__(self):
+        return self.value
+
+class SOValidationTestFloat(SOValidationTest):
+    def __float__(self):
+        return self.value
 
 class TestValidation:
 
@@ -36,6 +58,14 @@ class TestValidation:
         raises(validators.Invalid, setattr, t, 'name7', 1)
         t.name2 = 'you'
         assert t.name2 == 'you'
+
+        for name, cls, value in (
+                ('name4', SOValidationTestFloat, 1.1),
+                ('name6', SOValidationTestBool, True),
+                ('name7', SOValidationTestUnicode, u'test'),
+                ('name8', SOValidationTestInt, 1)):
+            setattr(t, name, cls(value))
+            assert getattr(t, name) == value
 
     def test_wrapType(self):
         t = SOValidation(name3=1)
