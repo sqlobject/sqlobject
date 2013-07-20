da4ab0e8aabcf2ae078960aecedbb4f1c162c1c9
Oleg Broytman <phd@phdru.name>
2013-07-20 17:01:38 +0000

    PyPI has switched to https
    git-svn-id: http://svn.colorstudy.com/SQLObject/branches/1.5@4625 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ For development see the `subversion repository
       author="Ian Bicking",
       author_email="ianb@colorstudy.com",
       url="http://sqlobject.org/",
-      download_url="http://pypi.python.org/pypi/SQLObject/%s" % version,
+      download_url="https://pypi.python.org/pypi/SQLObject/%s" % version,
       license="LGPL",
       packages=["sqlobject"] + ['sqlobject.%s' % package for package in subpackages],
       scripts=["scripts/sqlobject-admin", "scripts/sqlobject-convertOldURI"],
