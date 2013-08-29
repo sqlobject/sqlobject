af089454ed3058d39a82ba5affea5f76a06265eb
Oleg Broytman <phd@phdru.name>
2013-08-29 20:12:01 +0000

    Release 1.5.0rc1
    git-svn-id: http://svn.colorstudy.com/SQLObject/tags/1.5.0rc1@4659 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ For development see the `subversion repository
       author="Ian Bicking",
       author_email="ianb@colorstudy.com",
       url="http://sqlobject.org/",
-      download_url="https://pypi.python.org/pypi/SQLObject/%s" % version,
+      download_url="https://pypi.python.org/pypi/SQLObject/1.5.0rc1dev-r4659",
       license="LGPL",
       packages=["sqlobject"] + ['sqlobject.%s' % package for package in subpackages],
       scripts=["scripts/sqlobject-admin", "scripts/sqlobject-convertOldURI"],
--- sqlobject/__version__.py
+++ sqlobject/__version__.py
@@ -1,8 +1,8 @@
 
-version = '1.5'
+version = '1.5.0rc1'
 major   = 1
 minor   = 5
 micro   = 0
-release_level = 'branch'
-serial  = 0
+release_level = 'release candidate'
+serial  = 1
 version_info = (major, minor, micro, release_level, serial)
