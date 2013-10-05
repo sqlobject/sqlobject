24cb9cf53b6a5e48696b5f83e5ce629488c37211
Oleg Broytman <phd@phdru.name>
2013-10-05 12:37:20 +0000

    SQLObject 1.5.0 final
    git-svn-id: http://svn.colorstudy.com/SQLObject/tags/1.5.0@4672 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/__version__.py
+++ sqlobject/__version__.py
@@ -1,8 +1,8 @@
 
-version = '1.5'
+version = '1.5.0'
 major   = 1
 minor   = 5
 micro   = 0
-release_level = 'branch'
+release_level = 'final'
 serial  = 0
 version_info = (major, minor, micro, release_level, serial)
