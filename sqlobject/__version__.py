1215de39cbc0dcc6d96c3b3338e7eb75e4debc57
Oleg Broytman <phd@phdru.name>
2013-12-25 15:40:32 +0000

    Going to release version 1.6.0a1
    git-svn-id: http://svn.colorstudy.com/SQLObject/tags/1.6.0a1@4700 95a46c32-92d2-0310-94a5-8d71aeb3d4b3

--- sqlobject/__version__.py
+++ sqlobject/__version__.py
@@ -1,8 +1,8 @@
 
-version = '1.6'
+version = '1.6.0a1'
 major   = 1
 minor   = 6
 micro   = 0
-release_level = 'branch'
-serial  = 0
+release_level = 'alpha'
+serial  = 1
 version_info = (major, minor, micro, release_level, serial)
