73432002e2a779fb7cc537dfbe62931c3e257033
Oleg Broytman <phd@phdru.name>
2014-12-11 14:59:29 +0300

    Release 1.6.3
    [skip ci]

--- sqlobject/__version__.py
+++ sqlobject/__version__.py
@@ -1,8 +1,8 @@
 
-version = '1.6.2'
+version = '1.6.3'
 major   = 1
 minor   = 6
-micro   = 2
+micro   = 3
 release_level = 'final'
 serial  = 0
 version_info = (major, minor, micro, release_level, serial)
