803eb31bdbf6e1483d3a5789fd25a175279bc652
Oleg Broytman <phd@phdru.name>
2014-12-14 17:13:48 +0300

    Release 1.7.2

--- README.txt
+++ README.txt
@@ -1,4 +1,4 @@
-SQLObject 1.7.1
+SQLObject 1.7.2
 ===============
 
 Thanks for looking at SQLObject.  SQLObject is an object-relational
--- sqlobject/__version__.py
+++ sqlobject/__version__.py
@@ -1,8 +1,8 @@
 
-version = '1.7.1'
+version = '1.7.2'
 major   = 1
 minor   = 7
-micro   = 1
+micro   = 2
 release_level = 'final'
 serial  = 0
 version_info = (major, minor, micro, release_level, serial)
