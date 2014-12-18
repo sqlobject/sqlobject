6e8d147f054b0ba0616a5a10bffc8b4545a2602d
Oleg Broytman <phd@phdru.name>
2014-12-18 23:25:59 +0300

    Python 2.5 or higher is required
    [skip ci]

--- setup.py
+++ setup.py
@@ -52,6 +52,7 @@ more abstract, and provides substantial database independence for
 applications.
 
 Supports MySQL, PostgreSQL, SQLite, Firebird, Sybase, MSSQL and MaxDB (SAPDB).
+Python 2.5 or higher is required.
 
 For development see the projects at
 `SourceForge <http://sourceforge.net/projects/sqlobject/>`_
