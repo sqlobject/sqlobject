96a6e8838978f2d39efa6913676298f044729eab
Oleg Broytman <phd@phdru.name>
2015-01-06 18:42:49 +0300

    Branch 2.0 is no longer maintained

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ and `GitHub <https://github.com/sqlobject>`_.
   :target: https://travis-ci.org/sqlobject/sqlobject
 """,
       classifiers=[
-        "Development Status :: 5 - Production/Stable",
+        "Development Status :: 7 - Inactive",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
         "Programming Language :: Python",
