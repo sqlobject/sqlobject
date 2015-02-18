724ede91ada4bc064a8014630e0fe570eba85e3d
Neil <drnlmuller+sqlobject@gmail.com>
2015-02-18 10:47:38 +0200

    Add compataibility definitions

--- sqlobject/compat.py
+++ sqlobject/compat.py
@@ -1,3 +1,6 @@
+import sys
+import types
+
 # Credit to six authors: http://pypi.python.org/pypi/six
 # License: MIT
 
@@ -12,3 +15,11 @@ def with_metaclass(meta, *bases):
         def __new__(cls, name, this_bases, d):
             return meta(name, bases, d)
     return type.__new__(metaclass, 'temporary_class', (), {})
+
+# Compatability definitions (inspired by six)
+if sys.version_info[0] < 3:
+    string_type = basestring
+    class_types = (type, types.ClassType)
+else:
+    string_type = str
+    class_types = (type, )
