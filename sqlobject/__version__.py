4ca8a6d8dff5892d01ff4150f099c82a31f78b30
Oleg Broytman <phd@phdru.name>
2015-03-15 06:49:01 +0300

    Release 1.7.5

--- sqlobject/__version__.py
+++ sqlobject/__version__.py
@@ -1,8 +1,8 @@
 
-version = '1.7.4'
+version = '1.7.5'
 major   = 1
 minor   = 7
-micro   = 4
+micro   = 5
 release_level = 'final'
 serial  = 0
 version_info = (major, minor, micro, release_level, serial)
