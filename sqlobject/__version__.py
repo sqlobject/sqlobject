c720a21c04024972cee5497a45eb0d5ed201b217
Oleg Broytman <phd@phdru.name>
2016-05-08 09:53:05 +0300

    Release 2.2.0

--- sqlobject/__version__.py
+++ sqlobject/__version__.py
@@ -1,8 +1,8 @@
 
-version = '2.2.0b2'
+version = '2.2.0'
 major = 2
 minor = 2
 micro = 0
-release_level = 'beta'
-serial = 2
+release_level = 'final'
+serial = 0
 version_info = (major, minor, micro, release_level, serial)
