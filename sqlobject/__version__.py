215015c375813066bccec69afc4f84025ecc895d
Oleg Broytman <phd@phdru.name>
2016-07-10 16:52:26 +0300

    The next release will be 3.1.0

--- sqlobject/__version__.py
+++ sqlobject/__version__.py
@@ -1,8 +1,8 @@
 
-version = '3.0.0'
+version = '3.1.0'
 major = 3
-minor = 0
+minor = 1
 micro = 0
-release_level = 'final'
+release_level = 'trunk'
 serial = 0
 version_info = (major, minor, micro, release_level, serial)
