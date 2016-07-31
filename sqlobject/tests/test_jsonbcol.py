f50c991fc8afa95839f54af693bf3e030a8b90dc
Oleg Broytman <phd@phdru.name>
2016-07-31 17:28:27 +0300

    Fix test_jsonbcol.py
    Skip the test if non-postgres connection. Fix 'import *'.
    Fix formatting (flake8 warnings). Run tests at Travis with Pg 9.4.

--- sqlobject/tests/test_jsonbcol.py
+++ sqlobject/tests/test_jsonbcol.py
@@ -1,6 +1,6 @@
-from json import loads, dumps
-from sqlobject import *
-from sqlobject.tests.dbtest import *
+import py.test
+from sqlobject import SQLObject, JsonbCol
+from sqlobject.tests.dbtest import getConnection, setupClass
 
 
 ########################################
@@ -14,26 +14,32 @@ class JsonbContainer(SQLObject):
     jsondata_list = JsonbCol(default=None)
 
 
-dictdata = dict(ship=u'USS Voyager',
-        number='NCC-74656',
-        crew=['Cpt. Janeway', 'Doctor', 'Lt. Torries', 'Seven of Nine'],
-        races=('Borg','Kason',),
-        distance2earth=70000,
-        coming_home=True)
+dictdata = dict(
+    ship=u'USS Voyager',
+    number='NCC-74656',
+    crew=['Cpt. Janeway', 'Doctor', 'Lt. Torries', 'Seven of Nine'],
+    races=('Borg', 'Kason',),
+    distance2earth=70000,
+    coming_home=True)
 
 nonedata = None
 
 listofdictsdata = [dict(Erath=7390000000),
-                    dict(Vulcan=6000000000),
-                    dict(Kronos='unknown')]
+                   dict(Vulcan=6000000000),
+                   dict(Kronos='unknown')]
 
 
 def test_jsonbCol():
+    connection = getConnection()
+    if connection.dbName != "postgres":
+        py.test.skip("These tests require PostgreSQL")
+
     setupClass([JsonbContainer], force=True)
 
-    my_jsonb = JsonbContainer(jsondata=dictdata, 
-            jsondata_none=nonedata,
-            jsondata_list=listofdictsdata)
+    my_jsonb = JsonbContainer(
+        jsondata=dictdata,
+        jsondata_none=nonedata,
+        jsondata_list=listofdictsdata)
     iid = my_jsonb.id
 
     JsonbContainer._connection.cache.clear()
@@ -42,6 +48,6 @@ def test_jsonbCol():
 
     assert my_jsonb_2.jsondata['coming_home'] == dictdata['coming_home']
     assert my_jsonb_2.jsondata['crew'] == dictdata['crew']
-    assert my_jsonb_2.jsondata['races'] == ['Borg','Kason']
-    assert my_jsonb_2.jsondata_list == listofdictsdata
+    assert my_jsonb_2.jsondata['races'] == ['Borg', 'Kason']
     assert my_jsonb_2.jsondata_none is None
+    assert my_jsonb_2.jsondata_list == listofdictsdata
