eca92f170393fda5624671ac0e4fa88953e05bad
Oleg Broytman <phd@phdru.name>
2017-01-04 09:45:48 +0300

    Remove test where dbEncoding for a column differs from database

--- sqlobject/tests/test_unicode.py
+++ sqlobject/tests/test_unicode.py
@@ -1,7 +1,6 @@
-import pytest
-from sqlobject import AND, IntCol, OR, SQLObject, UnicodeCol
+from sqlobject import IntCol, SQLObject, UnicodeCol
 from sqlobject.compat import PY2
-from sqlobject.tests.dbtest import raises, setupClass
+from sqlobject.tests.dbtest import setupClass
 
 
 ########################################
@@ -11,9 +10,7 @@ from sqlobject.tests.dbtest import raises, setupClass
 
 class SOTestUnicode(SQLObject):
     count = IntCol(alternateID=True)
-    col1 = UnicodeCol(alternateID=True, length=100)
-    if PY2:
-        col2 = UnicodeCol(dbEncoding='latin1')
+    col = UnicodeCol(alternateID=True, length=100)
 
 
 data = [u'\u00f0', u'test', 'ascii test']
@@ -24,118 +21,62 @@ def setup():
     global items
     items = []
     setupClass(SOTestUnicode)
-    if SOTestUnicode._connection.dbName == 'postgres':
-        if PY2:
-            dbEncoding = 'latin1'
-        else:
-            dbEncoding = 'utf8'
-        SOTestUnicode._connection.query(
-            'SET client_encoding TO %s' % dbEncoding)
-    if PY2:
-        for i, s in enumerate(data):
-            items.append(SOTestUnicode(count=i, col1=s, col2=s))
-    else:
-        for i, s in enumerate(data):
-            items.append(SOTestUnicode(count=i, col1=s))
+    for i, s in enumerate(data):
+        items.append(SOTestUnicode(count=i, col=s))
 
 
 def test_create():
     setup()
     for s, item in zip(data, items):
-        assert item.col1 == s
-        if PY2:
-            assert item.col2 == s
+        assert item.col == s
 
     conn = SOTestUnicode._connection
     if PY2:
         rows = conn.queryAll("""
-        SELECT count, col1, col2
+        SELECT count, col
         FROM so_test_unicode
         ORDER BY count
         """)
-        for count, col1, col2 in rows:
-            assert data[count].encode('utf-8') == col1
-            assert data[count].encode('latin1') == col2
+        for count, col in rows:
+            assert data[count].encode('utf-8') == col
     else:
         rows = conn.queryAll("""
-        SELECT count, col1
+        SELECT count, col
         FROM so_test_unicode
         ORDER BY count
         """)
         # On python 3, everthings already decoded to unicode
-        for count, col1 in rows:
-            assert data[count] == col1
-
-
-def _test_select():
-    for i, value in enumerate(data):
-        rows = list(SOTestUnicode.select(SOTestUnicode.q.col1 == value))
-        assert len(rows) == 1
-        if PY2:
-            rows = list(SOTestUnicode.select(SOTestUnicode.q.col2 == value))
-            assert len(rows) == 1
-            rows = list(SOTestUnicode.select(AND(
-                SOTestUnicode.q.col1 == value,
-                SOTestUnicode.q.col2 == value
-            )))
-            assert len(rows) == 1
-        rows = list(SOTestUnicode.selectBy(col1=value))
-        assert len(rows) == 1
-        if PY2:
-            rows = list(SOTestUnicode.selectBy(col2=value))
-            assert len(rows) == 1
-            rows = list(SOTestUnicode.selectBy(col1=value, col2=value))
-            assert len(rows) == 1
-        row = SOTestUnicode.byCol1(value)
-        assert row.count == i
-    if PY2:
-        rows = list(SOTestUnicode.select(OR(
-            SOTestUnicode.q.col1 == u'\u00f0',
-            SOTestUnicode.q.col2 == u'test'
-        )))
-        assert len(rows) == 2
-        rows = list(SOTestUnicode.selectBy(col1=u'\u00f0', col2=u'test'))
-        assert len(rows) == 0
-
-    # starts/endswith/contains
-    rows = list(SOTestUnicode.select(SOTestUnicode.q.col1.startswith("test")))
-    assert len(rows) == 1
-    rows = list(SOTestUnicode.select(SOTestUnicode.q.col1.endswith("test")))
-    assert len(rows) == 2
-    rows = list(SOTestUnicode.select(SOTestUnicode.q.col1.contains("test")))
-    assert len(rows) == 2
-    rows = list(SOTestUnicode.select(
-        SOTestUnicode.q.col1.startswith(u"\u00f0")))
-    assert len(rows) == 1
-    rows = list(SOTestUnicode.select(SOTestUnicode.q.col1.endswith(u"\u00f0")))
-    assert len(rows) == 1
-    rows = list(SOTestUnicode.select(SOTestUnicode.q.col1.contains(u"\u00f0")))
-    assert len(rows) == 1
+        for count, col in rows:
+            assert data[count] == col
 
 
 def test_select():
     setup()
-    _test_select()
+    for i, value in enumerate(data):
+        rows = list(SOTestUnicode.select(SOTestUnicode.q.col == value))
+        assert len(rows) == 1
+        if PY2:
+            rows = list(SOTestUnicode.select(SOTestUnicode.q.col == value))
+            assert len(rows) == 1
+        rows = list(SOTestUnicode.selectBy(col=value))
+        assert len(rows) == 1
+        if PY2:
+            rows = list(SOTestUnicode.selectBy(col=value))
+            assert len(rows) == 1
+        row = SOTestUnicode.byCol(value)
+        assert row.count == i
 
-
-def test_dbEncoding():
-    if not PY2:
-        # Python 3 mostly ignores dbEncoding
-        pytest.skip("This test is for python 2")
-    setup()
-    SOTestUnicode.sqlmeta.dbEncoding = 'utf-8'
-    _test_select()
-    SOTestUnicode.sqlmeta.dbEncoding = 'latin-1'
-    raises(AssertionError, _test_select)
-    SOTestUnicode.sqlmeta.dbEncoding = 'ascii'
-    raises(UnicodeEncodeError, _test_select)
-    SOTestUnicode.sqlmeta.dbEncoding = None
-
-    SOTestUnicode._connection.dbEncoding = 'utf-8'
-    _test_select()
-    SOTestUnicode._connection.dbEncoding = 'latin-1'
-    raises(AssertionError, _test_select)
-    SOTestUnicode._connection.dbEncoding = 'ascii'
-    raises(UnicodeEncodeError, _test_select)
-    del SOTestUnicode.sqlmeta.dbEncoding
-    SOTestUnicode._connection.dbEncoding = 'utf-8'
+    # starts/endswith/contains
+    rows = list(SOTestUnicode.select(SOTestUnicode.q.col.startswith("test")))
+    assert len(rows) == 1
+    rows = list(SOTestUnicode.select(SOTestUnicode.q.col.endswith("test")))
+    assert len(rows) == 2
+    rows = list(SOTestUnicode.select(SOTestUnicode.q.col.contains("test")))
+    assert len(rows) == 2
+    rows = list(SOTestUnicode.select(
+        SOTestUnicode.q.col.startswith(u"\u00f0")))
+    assert len(rows) == 1
+    rows = list(SOTestUnicode.select(SOTestUnicode.q.col.endswith(u"\u00f0")))
+    assert len(rows) == 1
+    rows = list(SOTestUnicode.select(SOTestUnicode.q.col.contains(u"\u00f0")))
+    assert len(rows) == 1
