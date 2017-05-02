f09ab7a6c734672d50df14e8d3f364c458eb3ce5
Oleg Broytman <phd@phdru.name>
2017-05-02 22:11:15 +0300

    Fix split.sh: recognize both 'c' and 'rc'
    [skip ci]

--- devscripts/test-split.sh
+++ devscripts/test-split.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 . `dirname $0`/split.sh &&
-split_tag 21.12.42c4
 
 test_eq() {
    if [ "$1" != "$2" ]; then
@@ -9,6 +8,15 @@ test_eq() {
    fi
 }
 
+split_tag 21.12.42c4
+test_eq "$branch" 21.12
+test_eq "$major" 21
+test_eq "$minor" 12
+test_eq "$micro" 42
+test_eq "$state" "release candidate"
+test_eq "$serial" 4
+
+split_tag 21.12.42rc4
 test_eq "$branch" 21.12
 test_eq "$major" 21
 test_eq "$minor" 12
