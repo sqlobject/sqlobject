0266ac96332b82b768afd8344f8a6a2ea8fe1ef2
Oleg Broytman <phd@phdru.name>
2020-05-04 21:38:06 +0300

    Tests: Add `test-sqlobject.cmd`

---
+++ devscripts/test-sqlobject.cmd
@@ -0,0 +1,16 @@
+@echo off
+
+SetLocal EnableDelayedExpansion
+set SavePATH=%PATH%
+
+for %%V in (27 34 35 36 37 38) do (
+   for %%s in (32 64) do (
+      set PATH=C:\Python%%V-%%s;C:\Python%%V-%%s\Scripts;!SavePATH!
+      set TOXPYTHON=C:\Python%%V-%%s\python.exe
+      !TOXPYTHON! -m tox -e "py%%V-sqlite{-memory,}-w32"
+      if !ERRORLEVEL! EQU 0 (echo Ok) else (echo Error && goto Quit)
+   )
+)
+
+:Quit
+set PATH=%SavePATH%
