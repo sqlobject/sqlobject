6d5d0c200b8428b009a35d8f4c5912413e5db7c6
Oleg Broytman <phd@phdru.name>
2020-09-13 16:17:01 +0300

    Test(mysql/json): Test JSON functions
    Refs: #159.

--- sqlobject/tests/test_jsoncol.py
+++ sqlobject/tests/test_jsoncol.py
@@ -1,3 +1,4 @@
+import pytest
 from sqlobject import SQLObject, JSONCol
 from sqlobject.tests.dbtest import setupClass
 
@@ -18,17 +19,38 @@ _json_test_data = (
      u"unicode", u"unicode'with'apostrophes", u"unicode\"with\"quotes",
      ],
     u"unicode", u"unicode'with'apostrophes", u"unicode\"with\"quotes",
+    {'test': 'Test'},
 )
 
 
-def test_JSONCol():
+def _setup():
     setupClass(JSONTest)
 
     for _id, test_data in enumerate(_json_test_data):
-        json = JSONTest(id=_id + 1, json=test_data)
+        JSONTest(id=_id + 1, json=test_data)
 
+
+def test_JSONCol():
+    _setup()
     JSONTest._connection.cache.clear()
 
     for _id, test_data in enumerate(_json_test_data):
         json = JSONTest.get(_id + 1)
         assert json.json == test_data
+
+
+def test_JSONCol_funcs():
+    connection = JSONTest._connection
+    if not hasattr(connection, 'can_use_json_funcs') \
+            or not connection.can_use_json_funcs():
+        pytest.skip(
+            "The database doesn't support JSON functions; "
+            "JSON functions are supported by MariaDB since version 10.2.7 "
+            "and by MySQL since version 5.7.")
+    _setup()
+
+    rows = list(
+        JSONTest.select(JSONTest.q.json.json_extract('test') == 'Test')
+    )
+    assert len(rows) == 1
+    assert rows[0].json == {'test': 'Test'}
