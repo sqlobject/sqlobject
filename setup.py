48b1521d3fd55fb1bd968d6fdd34656e471004b1
Oleg Broytman <phd@phdru.name>
2020-11-24 18:43:16 +0300

    Python 3.9 is supported

--- setup.py
+++ setup.py
@@ -60,6 +60,7 @@ and `GitHub <https://github.com/sqlobject>`_.
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Topic :: Database",
         "Topic :: Database :: Front-Ends",
         "Topic :: Software Development :: Libraries :: Python Modules",
