1aed26838d1616b3686b74697e01bb4da5e47b79
Oleg Broytman <phd@phdru.name>
2022-09-16 23:37:31 +0300

    Style: Fix `flake8` E275 missing whitespace after keyword

--- sqlobject/tests/test_identity.py
+++ sqlobject/tests/test_identity.py
@@ -20,10 +20,10 @@ def test_identity():
     SOTestIdentity(n=100)  # i1
     # verify result
     i1get = SOTestIdentity.get(1)
-    assert(i1get.n == 100)
+    assert (i1get.n == 100)
 
     # insert while giving identity
     SOTestIdentity(id=2, n=200)  # i2
     # verify result
     i2get = SOTestIdentity.get(2)
-    assert(i2get.n == 200)
+    assert (i2get.n == 200)
